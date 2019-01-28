1008dfd238d3f07377498bd3a79266278124c5af
Andrew <mykge@outlook.com>
2019-01-28 14:17:37 +0800

    add codes

---
+++ src/StandardCore.Routine.WaitGroup/WaitGroup.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+
+namespace StandardCore.Routine
+{
+    public class WaitGroup
+    {
+        private int _counter = 0;
+
+        public void Add(uint delta)
+        {
+            if(delta == 1) Interlocked.Increment(ref _counter);
+            else
+            {
+                for(var i = 0; i < delta; i++)
+                {
+                    Interlocked.Increment(ref _counter);
+                }
+            }
+        }
+
+        public void Done()
+        {
+            Interlocked.Decrement(ref _counter);
+        }
+
+        public void Await()
+        {
+            while (_counter > 0) continue;
+        }
+    }
+}
---
+++ test/WaitGroupTest/WaitGroupTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StandardCore.Routine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace WaitGroupTest
+{
+    [TestClass]
+    public class WaitGroupTests
+    {
+        [TestMethod]
+        public void NoWaitOnZero()
+        {
+            var wg = new WaitGroup();
+
+            wg.Await();
+        }
+
+        [TestMethod]
+        public void Await()
+        {
+            var wg = new WaitGroup();
+            var i = new byte[] { 0 };
+            wg.Add(2);
+            new Thread(new ParameterizedThreadStart((state) =>
+            {
+                Thread.Sleep(100);
+                i[0] = (byte)(i[0] + 1);
+                wg.Done();
+            })).Start();
+            new Thread(new ParameterizedThreadStart((state) =>
+            {
+                Thread.Sleep(100);
+                i[0] = (byte)(i[0] + 1);
+                wg.Done();
+            })).Start();
+
+            wg.Await();
+            Assert.AreNotEqual(i[0], 0);
+        }
+    }
+}
